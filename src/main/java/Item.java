9da0c905e24ee51406125821a67df29bc3a99fca
Gerberg <masi0001@stud.kea.dk>
2023-09-29 14:02:19 +0200

    New item names

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -1,18 +1,27 @@
 public class Item {
     private String name;
+    private String longName;
     private String description;
-    public Item(String name, String description){
+    public Item(String name, String longname, String description){
         this.name = name;
+        this.longName = longName;
         this.description = description;
     }
     public String getName() {
         return name;
+    }
+    public String getLongName() {
+        return longName;
     }
     public String getDescription(){
         return description;
     }
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void setLongName(String longName){
+        this.longName = longName;
     }
     public void setDescription(String description){
         this.description = description;
